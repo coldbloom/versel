35db801bc83186ece1a180a0e80b36cf5dbcb901
unknown <coldbloom22@gmail.com>
2022-10-08 15:38:32 +0300

    ask

---
+++ Components/AskAndAnswer/ChildComponent.js
@@ -0,0 +1,42 @@
+import React, {useState, useRef} from 'react'
+import {FiChevronRight} from 'react-icons/fi'
+import s from './AskAndAnswer.module.css'
+import cn from 'classnames'
+
+
+const ChildComponent = ({data}) => {
+    const accordion = useRef(null);
+    const [open, setOpen] = useState(false)
+    const [height, setHeight] = useState('0px')
+
+    const openAnswer = () => {
+        setOpen(!open)
+        if (open === true) {
+            setHeight('0px')
+        } else {
+            setHeight(`${accordion.current.scrollHeight}px`)
+        }
+    }
+    return (
+        <div className={cn('border rounded mt-3 ', s.color)}>
+            <button
+                className={open
+                    ? cn('flex flex-row items-center py-4 md:px-4 px-1 w-full rounded justify-center', s.button, s.button__active)
+                    : cn('flex flex-row items-center py-4 md:px-4 px-1 w-full justify-center', s.button)
+                }
+                onClick={openAnswer}>
+                <h3 className=''>{data.question}</h3>
+                <div>
+                    <FiChevronRight size={22} className={open ? cn(s.rotate, s.transition) : s.transition}/>
+                </div>
+            </button>
+            <div className={cn(s.accordion, 'px-4')} ref={accordion} style={{maxHeight: height}}>
+                <div className='pt-1'/>
+                {data.answer}
+                <div className='pt-1'/>
+            </div>
+        </div>
+    )
+}
+
+export default ChildComponent
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
+import AskAndAnswer from "../Components/AskAndAnswer/AskAndAnswer";
 
 export default function Home() {
   return (
@@ -12,20 +13,11 @@ export default function Home() {
       </Head>
 
       <main className={styles.main}>
-        <h1>Vercel Test</h1>
+        <AskAndAnswer />
       </main>
 
       <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <span className={styles.logo}>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-          </span>
-        </a>
+
       </footer>
     </div>
   )
