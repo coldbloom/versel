b1187a6b1d8ee339523e6ec567d39c54e2d11c89
konstantin.kolesnik <Konstan.Kolesnik@x5.ru>
2024-11-21 21:17:12 +0300

    add robots.txt and sitemap

---
+++ next-sitemap.config.js
@@ -0,0 +1,6 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: 'https://example.com', // Замените на URL вашего сайта
+  generateRobotsTxt: true, // (опционально) Генерация robots.txt
+  // Дополнительные настройки
+}
